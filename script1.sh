85091ae535d79a81a9f892017acc7683ff4bcda1
Karim <karimausiasmarch@gmail.com>
2025-10-29 19:53:57 +0100

    vpc y subred listas

--- script1.sh
+++ script1.sh
@@ -3,7 +3,19 @@ VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
 --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPC_KARIM}]' \
 --query Vpc.VpcId --output text)
 
+echo $VPC_ID
+
 #habilito dns en la vpc
 aws ec2 modify-vpc-attribute \
     --vpc-id $VPC_ID \
     --enable-dns-hostnames "{\"Value\":true}"
+
+SUB_ID=$(aws ec2 create-subnet \
+    --vpc-id $VPC_ID \
+    --cidr-block 192.168.0.0/28 \
+    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=subred1-karim}]' \
+    --query Subnet.SubnetId --output text)
+
+echo $SUB_ID
+
+aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
