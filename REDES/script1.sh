b9cdeeb70d70dde8d5ddcaefd5e71a04400ffa5f
Karim <karimausiasmarch@gmail.com>
2025-11-06 16:33:47 +0100

    script1.sh modificado

--- REDES/script1.sh
+++ REDES/script1.sh
@@ -21,27 +21,53 @@ echo $SUB_ID
 aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
 
 
-#crear grupo de seguridad
-SG_ID=$(aws ec2 create-security-group --group-name gs-karim --description "grupo de seguridad karim" \
+# Creación del grupo de seguridad
+SG_ID=$(aws ec2 create-security-group --group-name My-SG \
+    --description "Mi grupito de seguridad - 22P" \
     --vpc-id $VPC_ID \
     --output text)
 
-#autorizar la entrada ssh
+SG_ID_ARN=$(echo $SG_ID | cut -d' ' -f2)
+SG_ID=$(echo $SG_ID | cut -d' ' -f1)
+
+## Autorización de abrir el puerto 22 para el grupo de seguridad
 aws ec2 authorize-security-group-ingress \
     --group-id $SG_ID \
     --protocol tcp \
     --port 22 \
-    --cidr 0.0.0.0/0
+    --cidr 0.0.0.0/0 > /dev/null
 
-#creo un ec2
+aws ec2 authorize-security-group-ingress \
+    --group-id $SG_ID \
+    --protocol tcp \
+    --port 80 \
+    --cidr 0.0.0.0/0 > /dev/null
+
+aws ec2 authorize-security-group-ingress \
+    --group-id $SG_ID \
+    --protocol tcp \
+    --port 443 \
+    --cidr 0.0.0.0/0 > /dev/null
+
+echo $SG_ID
+
+### Habilitar la asginación de IPv4 Pública en la subred
+aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch 
+
 EC2_ID=$(aws ec2 run-instances \
-    --image-id ami-0360c520857e3138f \
+    --image-id ami-0ecb62995f68bb549 \
     --instance-type t3.micro \
-    --key-name vockey \
     --subnet-id $SUB_ID \
+    --security-group-ids $SG_ID \
     --associate-public-ip-address \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=myEC2}]' \
+    --count 1 \
+    --key-name vockey \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Hector-Instance}]' \
     --query Instances.InstanceId --output text)
 
-sleep 15
+sleep 10m
+
 echo $EC2_ID
+
+# En caso de NO indicar el grupo de seguridad se haría de la siguiente manera:
+# aws ec2 modify-instance-attribute --instance-id $EC2_ID --groups $SG_ID
