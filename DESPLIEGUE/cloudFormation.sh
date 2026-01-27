eb6b063eee4b2d976f62f1dbdfad2ec7fe4c7b0a
Karim <karimausiasmarch@gmail.com>
2026-01-27 19:58:21 +0100

    script de cloud formation actalizado con las nuevas actividades

--- DESPLIEGUE/cloudFormation.sh
+++ DESPLIEGUE/cloudFormation.sh
@@ -43,5 +43,47 @@ aws cloudformation create-stack \
 aws cloudformation create-stack \
   --stack-name vpc-multiregion-karim \
   --template-body file:///home/karrezmou/Escritorio/plantillas-yml/aws4-02-2.yml \
+  --region us-east-1 \
   --parameters \
-  ParameterKey=AttachIGW,ParameterValue=true
+  ParameterKey=AttachIGW,ParameterValue=true 
+
+
+aws cloudformation create-stack \
+  --stack-name alb-asg \
+  --template-body file:///home/karrezmou/Escritorio/plantillas-yml/aws4-02-3.yml \
+  --parameters \
+  ParameterKey=InstanceType,ParameterValue=t3.micro \
+  ParameterKey=ALBPublico,ParameterValue=true \
+  ParameterKey=NombreBase,ParameterValue=vivalalandingkarim
+
+
+aws cloudformation create-stack \
+  --stack-name vpc-peering \
+  --template-body file:///home/karrezmou/Escritorio/plantillas-yml/aws4-03-1.yml \
+  --parameters \
+    ParameterKey=Ambiente,ParameterValue=Dev \
+    ParameterKey=VpcDestinoPeering,ParameterValue=vpc-0141d073ec666866a \
+    ParameterKey=PublicSubnet1Cidr,ParameterValue=20.0.1.0/24 \
+    ParameterKey=PublicSubnet2Cidr,ParameterValue=20.0.2.0/24 \
+    ParameterKey=PrivateSubnet1Cidr,ParameterValue=20.0.10.0/24 \
+    ParameterKey=PrivateSubnet2Cidr,ParameterValue=20.0.11.0/24
+
+aws cloudformation deploy \
+  --template-file /home/karrezmou/Escritorio/plantillas-yml/aws4-03-2.yml \
+  --stack-name stack-vpc-rds \
+  --parameter-overrides \
+    Ambiente=Dev \
+    VpcId=vpc-0317742a40d2b3841 \
+    PrivateSubnet1Id=subnet-04d2ab9ecb5222562 \
+    PrivateSubnet2Id=subnet-0bfce609d817c1cb5 \
+    DBEngineVersion=8.0.43 \
+    DBUsername=admin \
+    DBPassword=adminadmin
+
+aws cloudformation create-stack \
+  --stack-name stack-s3 \
+  --template-body file:///home/karrezmou/Escritorio/plantillas-yml/aws4-03-3.yml \
+  --parameters \
+    ParameterKey=AccessLevel,ParameterValue=Privado \
+    ParameterKey=BucketName,ParameterValue=prueba-s3-stack-karim \
+    ParameterKey=StorageType,ParameterValue=IntelligentTiering 
